67a9f4287246273a8ff7ce708fdb2f270c8b9307
Davi Maciel Ferreira <kmaciel.km@gmail.com>
2024-09-09 23:18:49 -0300

    Feat: Arquivos iniciais da aplicação Java Backend criados
    Arquivos base do spring boot criados, dependências adicionadas

---
+++ projetotransparencia/src/main/java/com/codenine/projetotransparencia/ProjetotransparenciaApplication.java
@@ -0,0 +1,13 @@
+package com.codenine.projetotransparencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetotransparenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetotransparenciaApplication.class, args);
+	}
+
+}
---
+++ projetotransparencia/src/test/java/com/codenine/projetotransparencia/ProjetotransparenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codenine.projetotransparencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetotransparenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
