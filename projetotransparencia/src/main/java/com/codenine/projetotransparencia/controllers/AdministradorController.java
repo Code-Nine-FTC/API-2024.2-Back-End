aac2bcb0ef7566ac5d427b0e28898a47a230c53d
Renato-Cruz-Jr <142221532+Renato-Cruz-Jr@users.noreply.github.com>
2024-09-11 10:48:01 -0300

    feat: atualizei o Controller do administrador

--- projetotransparencia/src/main/java/com/codenine/projetotransparencia/controllers/AdministradorController.java
+++ projetotransparencia/src/main/java/com/codenine/projetotransparencia/controllers/AdministradorController.java
@@ -1,4 +1,27 @@
+// controllers/AdministradorController.java
 package com.codenine.projetotransparencia.controllers;
 
+import com.codenine.projetotransparencia.entities.Administrador;
+import com.codenine.projetotransparencia.services.AdministradorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/administradores")
 public class AdministradorController {
+
+    @Autowired
+    private AdministradorService administradorService;
+
+    @PostMapping
+    public Administrador createAdministrador(@RequestBody Administrador administrador) {
+        return administradorService.createAdministrador(administrador);
+    }
+
+    @GetMapping
+    public List<Administrador> getAllAdministradores() {
+        return administradorService.getAllAdministradores();
+    }
 }
