fb84b7be8bac37ea3115b39ac0b2abe25a73eaf7
Renato-Cruz-Jr <142221532+Renato-Cruz-Jr@users.noreply.github.com>
2024-09-11 08:44:59 -0300

    feat: entidades Administrador e Projeto implementadas

--- projetotransparencia/src/main/java/com/codenine/projetotransparencia/entities/Administrador.java
+++ projetotransparencia/src/main/java/com/codenine/projetotransparencia/entities/Administrador.java
@@ -1,4 +1,20 @@
 package com.codenine.projetotransparencia.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class Administrador {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "administrador_id")
+    private Long administradorId;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
 }
--- projetotransparencia/src/main/java/com/codenine/projetotransparencia/entities/Projeto.java
+++ projetotransparencia/src/main/java/com/codenine/projetotransparencia/entities/Projeto.java
@@ -1,4 +1,43 @@
 package com.codenine.projetotransparencia.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Data
 public class Projeto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "projeto_id")
+    private Long projetoId;
+
+    @ManyToOne
+    @JoinColumn(name = "coordenador_id", referencedColumnName = "coordenador_id", nullable = false)
+    private Coordenador coordenador;
+
+    @Column(nullable = false)
+    private String nomeEmpresa;
+
+    @Column(nullable = false)
+    private String objeto;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(nullable = false)
+    private Double valor;
+
+    @Temporal(TemporalType.DATE)
+    @Column(nullable = false)
+    private Date dataInicio;
+
+    @Temporal(TemporalType.DATE)
+    @Column(nullable = false)
+    private Date dataTermino;
 }
