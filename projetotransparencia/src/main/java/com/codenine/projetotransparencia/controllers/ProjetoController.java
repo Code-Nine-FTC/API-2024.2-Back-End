f0f578f0a13e95c8cd785774995dc91cfaf0a4ad
Renato-Cruz-Jr <142221532+Renato-Cruz-Jr@users.noreply.github.com>
2024-09-11 10:48:42 -0300

    feat: atualizei o Controller do projeto

--- projetotransparencia/src/main/java/com/codenine/projetotransparencia/controllers/ProjetoController.java
+++ projetotransparencia/src/main/java/com/codenine/projetotransparencia/controllers/ProjetoController.java
@@ -1,4 +1,32 @@
+// controllers/ProjetoController.java
 package com.codenine.projetotransparencia.controllers;
 
+import com.codenine.projetotransparencia.entities.Projeto;
+import com.codenine.projetotransparencia.services.ProjetoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/projetos")
 public class ProjetoController {
+
+    @Autowired
+    private ProjetoService projetoService;
+
+    @PostMapping
+    public Projeto createProjeto(@RequestBody Projeto projeto) {
+        return projetoService.createProjeto(projeto);
+    }
+
+    @GetMapping
+    public List<Projeto> getAllProjetos() {
+        return projetoService.getAllProjetos();
+    }
+
+    @GetMapping("/{id}")
+    public Projeto getProjetoById(@PathVariable Long id) {
+        return projetoService.getProjetoById(id);
+    }
 }
