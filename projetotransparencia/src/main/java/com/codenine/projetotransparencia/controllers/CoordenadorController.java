021ee484b635c3d028d4f81bf2369c9f708b85ae
Renato-Cruz-Jr <142221532+Renato-Cruz-Jr@users.noreply.github.com>
2024-09-11 10:48:19 -0300

    feat: atualizei o controller do coordenador

--- projetotransparencia/src/main/java/com/codenine/projetotransparencia/controllers/CoordenadorController.java
+++ projetotransparencia/src/main/java/com/codenine/projetotransparencia/controllers/CoordenadorController.java
@@ -1,4 +1,32 @@
+// controllers/CoordenadorController.java
 package com.codenine.projetotransparencia.controllers;
 
+import com.codenine.projetotransparencia.entities.Coordenador;
+import com.codenine.projetotransparencia.services.CoordenadorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/coordenadores")
 public class CoordenadorController {
+
+    @Autowired
+    private CoordenadorService coordenadorService;
+
+    @PostMapping
+    public Coordenador createCoordenador(@RequestBody Coordenador coordenador) {
+        return coordenadorService.createCoordenador(coordenador);
+    }
+
+    @GetMapping
+    public List<Coordenador> getAllCoordenadores() {
+        return coordenadorService.getAllCoordenadores();
+    }
+
+    @GetMapping("/{id}")
+    public Coordenador getCoordenadorById(@PathVariable Long id) {
+        return coordenadorService.getCoordenadorById(id);
+    }
 }
