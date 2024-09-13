ab574d6a41771367d04feac295a65f775b972e95
JonasOliver <jonaasmigueldeoliveira@gmail.com>
2024-09-13 12:41:50 -0300

    fix: adicionei as rotas para requisição de criação do coordenador no controller do administrador

--- projetotransparencia/src/main/java/com/codenine/projetotransparencia/controllers/AdministradorController.java
+++ projetotransparencia/src/main/java/com/codenine/projetotransparencia/controllers/AdministradorController.java
@@ -2,7 +2,10 @@
 package com.codenine.projetotransparencia.controllers;
 
 import com.codenine.projetotransparencia.entities.Administrador;
+import com.codenine.projetotransparencia.entities.Coordenador;
 import com.codenine.projetotransparencia.services.AdministradorService;
+import com.codenine.projetotransparencia.services.CoordenadorService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,13 +18,27 @@ public class AdministradorController {
     @Autowired
     private AdministradorService administradorService;
 
-    @PostMapping
-    public Administrador createAdministrador(@RequestBody Administrador administrador) {
-        return administradorService.createAdministrador(administrador);
-    }
+    @Autowired
+    private CoordenadorService coordenadorService;
+
+    private static final String EMAIL_ADMIN_FIXO = "administradorAfpg@gmail.com";
 
     @GetMapping
     public List<Administrador> getAllAdministradores() {
         return administradorService.getAllAdministradores();
+    }
+
+    @PostMapping("/coordenadores")
+    public Coordenador createCoordenador(
+            @RequestParam String adminEmail,
+            @RequestBody Coordenador coordenador) {
+
+        
+        if (!adminEmail.equals(EMAIL_ADMIN_FIXO)) {
+            throw new RuntimeException("Apenas o administrador fixo pode cadastrar coordenadores.");
+        }
+
+        // Cadastra o coordenador
+        return coordenadorService.createCoordenador(coordenador);
     }
 }
