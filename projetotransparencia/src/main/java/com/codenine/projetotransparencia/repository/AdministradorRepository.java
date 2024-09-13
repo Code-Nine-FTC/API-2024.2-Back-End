48ca24b98b27ff486b5c731101a6b825cf5132be
JonasOliver <jonaasmigueldeoliveira@gmail.com>
2024-09-13 12:40:51 -0300

    fix: adicionei a utilização da verificação de email no repositorio do administrador

--- projetotransparencia/src/main/java/com/codenine/projetotransparencia/repository/AdministradorRepository.java
+++ projetotransparencia/src/main/java/com/codenine/projetotransparencia/repository/AdministradorRepository.java
@@ -7,5 +7,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AdministradorRepository extends JpaRepository<Administrador, Long> {
+	Administrador findByEmail(String email);
 }
 
