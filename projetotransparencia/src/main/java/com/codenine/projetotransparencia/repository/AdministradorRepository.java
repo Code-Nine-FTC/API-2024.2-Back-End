db37422a35808193085b83a7dd1693fa9d99ca11
JonasOliver <jonaasmigueldeoliveira@gmail.com>
2024-09-13 00:45:42 -0300

    fix: adicionei a notação de Repository nos repositorios de administrador e coordenador

--- projetotransparencia/src/main/java/com/codenine/projetotransparencia/repository/AdministradorRepository.java
+++ projetotransparencia/src/main/java/com/codenine/projetotransparencia/repository/AdministradorRepository.java
@@ -2,7 +2,10 @@ package com.codenine.projetotransparencia.repository;
 
 import com.codenine.projetotransparencia.entities.Administrador;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface AdministradorRepository extends JpaRepository<Administrador, Long> {
 }
 
--- projetotransparencia/src/main/java/com/codenine/projetotransparencia/repository/CoordenadorRepository.java
+++ projetotransparencia/src/main/java/com/codenine/projetotransparencia/repository/CoordenadorRepository.java
@@ -3,7 +3,9 @@ package com.codenine.projetotransparencia.repository;
 
 import com.codenine.projetotransparencia.entities.Coordenador;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CoordenadorRepository extends JpaRepository<Coordenador, Long> {
 }
 
