692ddc0272c3da03a2b3d7983abd4407389b06a1
JonasOliver <jonaasmigueldeoliveira@gmail.com>
2024-09-13 12:39:20 -0300

    fix: adicionei o get e set na entidade do coordenador

--- projetotransparencia/src/main/java/com/codenine/projetotransparencia/entities/Coordenador.java
+++ projetotransparencia/src/main/java/com/codenine/projetotransparencia/entities/Coordenador.java
@@ -27,4 +27,53 @@ public class Coordenador {
 
     @OneToMany(mappedBy = "coordenador", cascade = CascadeType.ALL)
     private List<Projeto> projetos;
+
+	public Long getCoordenadorId() {
+		return coordenadorId;
+	}
+
+	public void setCoordenadorId(Long coordenadorId) {
+		this.coordenadorId = coordenadorId;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getCoordenadorNome() {
+		return coordenadorNome;
+	}
+
+	public void setCoordenadorNome(String coordenadorNome) {
+		this.coordenadorNome = coordenadorNome;
+	}
+
+	public List<Projeto> getProjetos() {
+		return projetos;
+	}
+
+	public void setProjetos(List<Projeto> projetos) {
+		this.projetos = projetos;
+	}
+    
 }
