a1fa3425dfd748d3a9c7b1f8482a37bf08a4312a
JonasOliver <jonaasmigueldeoliveira@gmail.com>
2024-09-13 12:38:56 -0300

    fix: adicionei o get e set na entidade do administrador

--- projetotransparencia/src/main/java/com/codenine/projetotransparencia/entities/Administrador.java
+++ projetotransparencia/src/main/java/com/codenine/projetotransparencia/entities/Administrador.java
@@ -16,4 +16,28 @@ public class Administrador {
 
     @Column(nullable = false)
     private String senha;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 }
