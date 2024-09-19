91d6380e99aaec27a10c5d81c6b46b5e572b9908
Davi Maciel Ferreira <kmaciel.km@gmail.com>
2024-09-19 19:20:40 -0300

    Fix: Verificação de arquivos do tipo excel corrigida para tipos novos e antigos

--- projetotransparencia/src/main/java/com/codenine/projetotransparencia/utils/documents/VerificarExcel.java
+++ projetotransparencia/src/main/java/com/codenine/projetotransparencia/utils/documents/VerificarExcel.java
@@ -1,11 +1,41 @@
 package com.codenine.projetotransparencia.utils.documents;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 
 @Component
 public class VerificarExcel extends VerificarArquivos {
     @Override
     public boolean verificar(byte[] excel) {
-        return excel[0] == 0x50 && excel[1] == 0x4B && excel[2] == 0x03 && excel[3] == 0x04;
+        try (ByteArrayInputStream bis = new ByteArrayInputStream(excel)) {
+            if (isXLSX(bis) || isXLS(bis)) {
+                return true;
+            }
+        } catch (IOException e) {
+            return false;
+        }
+        return false;
+    }
+
+    private boolean isXLSX(ByteArrayInputStream bis) {
+        try {
+            WorkbookFactory.create(bis);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private boolean isXLS(ByteArrayInputStream bis) {
+        try {
+            new HSSFWorkbook(bis);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
