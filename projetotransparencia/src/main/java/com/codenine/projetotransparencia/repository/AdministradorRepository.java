05703cb2fbfce9d8f3959919fb9807af79f2fe94
JonasOliver <jonaasmigueldeoliveira@gmail.com>
2024-09-23 19:52:57 -0300

    Fix: Update no AdministradorRepository, para acesso através do tipo login

--- projetotransparencia/src/main/java/com/codenine/projetotransparencia/repository/AdministradorRepository.java
+++ projetotransparencia/src/main/java/com/codenine/projetotransparencia/repository/AdministradorRepository.java
@@ -1,12 +1,15 @@
 package com.codenine.projetotransparencia.repository;
 
-import com.codenine.projetotransparencia.entities.Administrador;
+
+import com.codenine.projetotransparencia.entities.AdministradorV1;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface AdministradorRepository extends JpaRepository<Administrador, Long> {
-	Administrador findByEmail(String email);
+public interface AdministradorRepository extends JpaRepository<AdministradorV1, String> {
+	 UserDetails findByLogin(String login);
 }
 
