896b2477afb12ca832ee2f07066eb43ef6447054
Davi Maciel Ferreira <104574671+DfMaciel@users.noreply.github.com>
2024-09-23 08:48:22 -0300

    Fix: Dto do cadastro do projeto atualizado

--- projetotransparencia/src/main/java/com/codenine/projetotransparencia/controllers/dto/AtualizarProjetoDto.java
+++ projetotransparencia/src/main/java/com/codenine/projetotransparencia/controllers/dto/AtualizarProjetoDto.java
@@ -1,21 +1,16 @@
 // controllers/AtualizarProjetoDto.java
 package com.codenine.projetotransparencia.controllers.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+
 import java.util.Date;
 import java.util.Optional;
 
 public record AtualizarProjetoDto(
         Long id, // Novo campo para o ID do projeto a ser atualizado
-        Optional<String> titulo,
-        Optional<String> referenciaProjeto,
-        Optional<String> empresa,
-        Optional<String> objeto,
-        Optional<String> descricao,
-        Optional<String> nomeCoordenador,
-        Optional<Double> valor,
-        Optional<Date> dataInicio,
-        Optional<Date> dataTermino,
+        @NotEmpty String projeto,
         Optional<byte[]> resumoPdf,
-        Optional<byte[]> resumoExcel
-) {
+        Optional<byte[]> resumoExcel,
+        Optional<byte[]> proposta,
+        Optional<byte[]> contrato){
 }
