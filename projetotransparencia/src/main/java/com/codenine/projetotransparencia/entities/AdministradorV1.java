3ed683df05a0ca81df7359e3f8d6e93ad5be5532
Pedro Martins de Oliveira <142221456+OliveiraPedro09@users.noreply.github.com>
2024-09-24 09:44:46 -0300

    Fix: Mudança do Nome para não criar uma tabela adicional
    Mudança no nome da tabela para não criar uma tabela adicional ao rodar o back-end.

--- projetotransparencia/src/main/java/com/codenine/projetotransparencia/entities/AdministradorV1.java
+++ projetotransparencia/src/main/java/com/codenine/projetotransparencia/entities/AdministradorV1.java
@@ -17,8 +17,8 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Table(name = "ADMIN")
-@Entity(name = "ADMIN")
+@Table(name = "administrador")
+@Entity(name = "administrador")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
