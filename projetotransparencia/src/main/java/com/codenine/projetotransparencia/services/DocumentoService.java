1814d092ff4df345d36a54e9c8e19d39e78286d2
Renato-Cruz-Jr <142221532+Renato-Cruz-Jr@users.noreply.github.com>
2024-09-26 14:12:46 -0300

    Feat: Função de downloads no DocumentoService criada

--- projetotransparencia/src/main/java/com/codenine/projetotransparencia/services/DocumentoService.java
+++ projetotransparencia/src/main/java/com/codenine/projetotransparencia/services/DocumentoService.java
@@ -1,5 +1,6 @@
 package com.codenine.projetotransparencia.services;
 
+import com.codenine.projetotransparencia.controllers.dto.BaixarDocumentoDto;
 import com.codenine.projetotransparencia.entities.Documento;
 import com.codenine.projetotransparencia.entities.Projeto;
 import com.codenine.projetotransparencia.repository.DocumentoRepository;
@@ -8,13 +9,17 @@ import com.codenine.projetotransparencia.utils.documents.SalvarDocumento;
 import com.codenine.projetotransparencia.utils.documents.VerificarExcel;
 import com.codenine.projetotransparencia.utils.documents.VerificarPdf;
 import com.codenine.projetotransparencia.utils.documents.VerificarTamanho;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.UrlResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.File;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 
@@ -84,6 +89,26 @@ public class DocumentoService {
 
     public List<Documento> listarDocumentos() {
         return documentoRepository.findAll();
+    }
+
+    public Documento visualizarDocumento(Long documentoId) {
+        Optional<Documento> documentoOptional = documentoRepository.findById(documentoId);
+        return documentoOptional.orElse(null);
+    }
+
+    public BaixarDocumentoDto baixarDocumento(Long documentoId) throws MalformedURLException {
+        Optional<Documento> documentoOptional = documentoRepository.findById(documentoId);
+        if (documentoOptional.isEmpty()) {
+            return null;
+        }
+        Documento documento = documentoOptional.get();
+        Path caminhoArquivo = Paths.get(relativePath).resolve(documento.getCaminho()).normalize();
+        Resource resource = new UrlResource(caminhoArquivo.toUri());
+        if (resource.exists()) {
+            return new BaixarDocumentoDto(resource, documento.getNome());
+        } else {
+            return null;
+        }
     }
 
     public void excluirDocumento(Long documentoId) {
