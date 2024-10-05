939c72785ef1c34959bb25968ffa2e1c77bac5ab
JonasOliver <jonaasmigueldeoliveira@gmail.com>
2024-10-05 16:02:13 -0300

    Fix: Removi codigo comentado, e adicionei um contador por nome de coordenador

--- projetotransparencia/src/main/java/com/codenine/projetotransparencia/repository/ProjetoRepository.java
+++ projetotransparencia/src/main/java/com/codenine/projetotransparencia/repository/ProjetoRepository.java
@@ -4,38 +4,26 @@ import com.codenine.projetotransparencia.entities.Projeto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
 import java.util.Date;
 import java.util.List;
-/*
+
 public interface ProjetoRepository extends JpaRepository<Projeto, Long> {
     @Query("SELECT p FROM Projeto p WHERE " +
-            "(:referencia IS NULL OR p.referencia LIKE %:referencia%) AND " +
-            "(:nomeCoordenador IS NULL OR p.nomeCoordenador LIKE %:nomeCoordenador%) AND " +
-            "(:dataInicio IS NULL OR p.dataInicio >= :dataInicio) AND " +
-            "(:dataTermino IS NULL OR p.dataTermino <= :dataTermino) AND" +
-            "(:valor IS NULL OR p.valor = :valor)"
-    )
-    List<Projeto> findByFiltros(@Param("referencia") String referencia,
-                                @Param("nomeCoordenador") String nomeCoordenador,
-                                @Param("dataInicio") Date dataInicio,
-                                @Param("dataTermino") Date dataTermino,
-                                @Param("valor") Double valor);
-}
-
-*/
-public interface ProjetoRepository extends JpaRepository<Projeto, Long> {
-         @Query("SELECT p FROM Projeto p WHERE " +
             "(:referencia IS NULL OR p.referencia LIKE %:referencia%) AND " +
             "(:nomeCoordenador IS NULL OR p.nomeCoordenador LIKE %:nomeCoordenador%) AND " +
             "(:dataInicio IS NULL OR p.dataInicio >= :dataInicio) AND " +
             "(:dataTermino IS NULL OR p.dataTermino <= :dataTermino) AND " +
             "(:valor IS NULL OR p.valor = :valor) " +
             "ORDER BY p.dataInicio ASC")
-        List<Projeto> findByFiltros(@Param("referencia") String referencia,
+    List<Projeto> findByFiltros(@Param("referencia") String referencia,
                                 @Param("nomeCoordenador") String nomeCoordenador,
                                 @Param("dataInicio") Date dataInicio,
                                 @Param("dataTermino") Date dataTermino,
                                 @Param("valor") Double valor);
 
-        long countByNomeCoordenador(String nomeCoordenador);
+    List<Projeto> findByReferencia(String referencia);
+
+    //   contar projetos pelo nome do coordenador
+    long countByNomeCoordenador(String nomeCoordenador);
 }
