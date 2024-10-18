2ad941e2bc971b7c2e38969717832f6a0343da07
JonasOliver <jonaasmigueldeoliveira@gmail.com>
2024-10-18 12:03:47 -0300

    Fix: Adicionei os parametros necessarios para busca por ano e meses

--- projetotransparencia/src/main/java/com/codenine/projetotransparencia/controllers/DashboardController.java
+++ projetotransparencia/src/main/java/com/codenine/projetotransparencia/controllers/DashboardController.java
@@ -3,35 +3,35 @@ package com.codenine.projetotransparencia.controllers;
 import com.codenine.projetotransparencia.services.DashboardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
-@RequestMapping("/dashboard")
 public class DashboardController {
 
     @Autowired
     private DashboardService dashboardService;
 
-    @GetMapping("/projetos/search")
-    public ResponseEntity<Map<String, Long>> getValues(
-        @RequestParam(required = false) String coordenador,
-        @RequestParam(required = false) String dataInicio,
-        @RequestParam(required = false) String dataTermino,
-        @RequestParam(required = false) String valorMaximo,
-        @RequestParam(required = false) String valorMinimo,
-        @RequestParam(required = false) String situacaoProjeto,
-        @RequestParam(required = false) String tipoBusca,
-        @RequestParam(required = false) String contratante) {
+    @GetMapping("/dashboard/projetos/search")
+    public ResponseEntity<Map<String, Long>> buscarProjetos(
+            @RequestParam(required = false) String coordenador,
+            @RequestParam(required = false) String dataInicio,
+            @RequestParam(required = false) String dataTermino,
+            @RequestParam(required = false) String valorMaximo,
+            @RequestParam(required = false) String valorMinimo,
+            @RequestParam(required = false) String situacaoProjeto,
+            @RequestParam(required = false) String tipoBusca,
+            @RequestParam(required = false) String contratante
 
-        // Chamando o método do serviço para contar os projetos dinamicamente
-        Map<String, Long> resultado = dashboardService.contarProjetosDinamicos(coordenador, dataInicio, dataTermino,
-                                                                             valorMaximo, valorMinimo,
-                                                                             situacaoProjeto, tipoBusca, contratante);
+    ) {
+        Map<String, Long> resultados = dashboardService.contarProjetosDinamicos(
+                dataInicio, dataTermino, coordenador, valorMaximo, valorMinimo,
+                situacaoProjeto, tipoBusca, contratante
+        );
 
-        // Retornando a contagem dos projetos
-        return ResponseEntity.ok(resultado);
+        return ResponseEntity.ok(resultados);
     }
 }
-
