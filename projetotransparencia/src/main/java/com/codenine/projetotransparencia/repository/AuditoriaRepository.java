a1cd54cf04ca101b99e1ce4f9ed4d5f5ed1606d4
Filipe Colla <filipe10colla@gmail.com>
2024-10-30 09:54:27 -0300

    Feat: Adicionada a busca por valores no repositorio de auditoria

--- projetotransparencia/src/main/java/com/codenine/projetotransparencia/repository/AuditoriaRepository.java
+++ projetotransparencia/src/main/java/com/codenine/projetotransparencia/repository/AuditoriaRepository.java
@@ -2,8 +2,18 @@ package com.codenine.projetotransparencia.repository;
 
 import com.codenine.projetotransparencia.entities.Auditoria;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AuditoriaRepository extends JpaRepository<Auditoria, Long> {
+
+    List<Auditoria> findByTipoAuditoria(String tipoAuditoria);
+
+    List<Auditoria> findByReferenciaProjeto(String referenciaProjeto);
+
+    List<Auditoria> findByTipoAuditoriaAndReferenciaProjeto(String tipoAuditoria, String referenciaProjeto);
 }
