387b792ec0e972218fb09c19a97f5d542e60b103
Filipe Colla <filipe10colla@gmail.com>
2024-10-30 09:55:48 -0300

    Feat: função de filtrar por valores feita no service

--- projetotransparencia/src/main/java/com/codenine/projetotransparencia/services/AuditoriaService.java
+++ projetotransparencia/src/main/java/com/codenine/projetotransparencia/services/AuditoriaService.java
@@ -2,17 +2,21 @@ package com.codenine.projetotransparencia.services;
 
 import com.codenine.projetotransparencia.entities.Auditoria;
 import com.codenine.projetotransparencia.repository.AuditoriaRepository;
+import com.codenine.projetotransparencia.repository.AuditoriaRepositoryCustomImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class AuditoriaService {
 
     @Autowired
     private AuditoriaRepository auditoriaRepository;
+
+    @Autowired
+    private AuditoriaRepositoryCustomImpl auditoriaRepositoryCustom;
+
 
     public List<Auditoria> listarAuditorias() {
         return auditoriaRepository.findAll();
@@ -21,5 +25,17 @@ public class AuditoriaService {
     // Método para buscar auditoria por ID
     public Optional<Auditoria> buscarAuditoriaPorId(Long id) {
         return auditoriaRepository.findById(id);
+    }
+
+    public List<Auditoria> buscarPorTipo(String tipoAuditoria) {
+        return auditoriaRepository.findByTipoAuditoria(tipoAuditoria);
+    }
+
+    public List<Auditoria> buscarPorReferencia(String referenciaProjeto) {
+        return auditoriaRepository.findByReferenciaProjeto(referenciaProjeto);
+    }
+
+    public List<Auditoria> buscarPorTipoEReferencia(String tipoAuditoria, String referenciaProjeto) {
+        return auditoriaRepository.findByTipoAuditoriaAndReferenciaProjeto(tipoAuditoria, referenciaProjeto);
     }
 }
