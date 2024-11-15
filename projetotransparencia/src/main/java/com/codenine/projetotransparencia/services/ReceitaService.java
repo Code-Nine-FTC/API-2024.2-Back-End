92e00fdcfec565de2f7b4306f95f01237eb1a62e
GitAutaza <173160383+collafilipe@users.noreply.github.com>
2024-11-15 18:23:38 -0300

    feat: Criado o service de Receita, com as funções de listar todos ou um especifico

---
+++ projetotransparencia/src/main/java/com/codenine/projetotransparencia/services/ReceitaService.java
@@ -0,0 +1,25 @@
+package com.codenine.projetotransparencia.services;
+
+import com.codenine.projetotransparencia.entities.Material;
+import com.codenine.projetotransparencia.entities.Receita;
+import com.codenine.projetotransparencia.repository.ReceitasRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ReceitaService {
+
+    @Autowired
+    private ReceitasRepository receitasRepository;
+
+    public List<Receita> listarReceita() {
+        return receitasRepository.findAll();
+    }
+
+    public Optional<Receita> buscarReceitaPorId(Long id) {
+        return receitasRepository.findById(id);
+    }
+}
