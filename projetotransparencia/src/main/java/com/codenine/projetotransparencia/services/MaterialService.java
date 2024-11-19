a31596cb498aa380d67f875cb9c5d951e998db6e
JonasOliver <jonaasmigueldeoliveira@gmail.com>
2024-11-19 10:59:50 -0300

    Fix: Atualizei o Material service, para que tenha relação com o MaterialDto

--- projetotransparencia/src/main/java/com/codenine/projetotransparencia/services/MaterialService.java
+++ projetotransparencia/src/main/java/com/codenine/projetotransparencia/services/MaterialService.java
@@ -2,6 +2,8 @@ package com.codenine.projetotransparencia.services;
 
 import com.codenine.projetotransparencia.entities.Bolsista;
 import com.codenine.projetotransparencia.entities.ClassificacaoDemanda;
+import com.codenine.projetotransparencia.controllers.dto.MaterialDto;
+import com.codenine.projetotransparencia.entities.Gasto;
 import com.codenine.projetotransparencia.entities.Material;
 import com.codenine.projetotransparencia.repository.MateriaisRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +18,35 @@ public class MaterialService {
     @Autowired
     private MateriaisRepository materiaisRepository;
 
-    public List<Material> listarMaterial() {
-        return materiaisRepository.findAll();
+
+    @Autowired
+    private GastoService gastoService; // Injeção do serviço de Gasto
+
+    public Material salvarMaterial(MaterialDto materialDTO) {
+        // Converter DTO para entidade
+        Material material = new Material();
+        material.setNome(materialDTO.getNome());
+        material.setValor(materialDTO.getValor());
+        material.setFornecedor(materialDTO.getFornecedor());
+        material.setFornecedorEmail(materialDTO.getFornecedorEmail());
+        material.setFornecedorTelefone(materialDTO.getFornecedorTelefone());
+        material.setStatusUtilizacao(materialDTO.getStatusUtilizacao());
+
+        // Se precisar associar um Gasto
+        if (materialDTO.getGastoId() != null) {
+            Gasto gasto = gastoService.buscarGastoPorId(materialDTO.getGastoId()); // Método corrigido
+            material.setGasto(gasto);
+        }
+
+        // Salvar no banco de dados
+        return materiaisRepository.save(material);
     }
 
     public Optional<Material> buscarMaterialPorId(Long id) {
         return materiaisRepository.findById(id);
+    }
+
+    public List<Material> listarMaterial() {
+        return materiaisRepository.findAll();
     }
 }
