6c99f30a0a57df904bc2bc4ea623bca6e7930f34
JonasOliver <jonaasmigueldeoliveira@gmail.com>
2024-11-19 11:00:22 -0300

    Fix: Atualizei o MaterialController

--- projetotransparencia/src/main/java/com/codenine/projetotransparencia/controllers/MaterialController.java
+++ projetotransparencia/src/main/java/com/codenine/projetotransparencia/controllers/MaterialController.java
@@ -1,10 +1,13 @@
 package com.codenine.projetotransparencia.controllers;
 
+import com.codenine.projetotransparencia.controllers.dto.MaterialDto;
 import com.codenine.projetotransparencia.entities.Bolsista;
 import com.codenine.projetotransparencia.entities.Material;
 import com.codenine.projetotransparencia.services.MaterialService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,11 +23,16 @@ public class MaterialController {
     @Autowired
     private MaterialService materialService;
 
+    // Endpoint para salvar o material
+    @PostMapping("/salvar")
+    public ResponseEntity<Material> salvarMaterial(@RequestBody @Valid MaterialDto materialDTO) {
+        Material material = materialService.salvarMaterial(materialDTO);
+        return ResponseEntity.status(201).body(material);
+    }
     @GetMapping("/listar")
     public List<Material> listarMaterial() {
         return materialService.listarMaterial();
     }
-
     @GetMapping("/visualizar/{id}")
     public ResponseEntity<Material> buscarMaterialPorId(@PathVariable Long id) {
         Optional<Material> material = materialService.buscarMaterialPorId(id);
