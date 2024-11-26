5eecfc4749474f1f88a67a98a88eb577fb9d6b24
Filipe Colla <filipe10colla@gmail.com>
2024-11-26 10:34:03 -0300

    Feat: Respositorio do convenio criada

---
+++ projetotransparencia/src/main/java/com/codenine/projetotransparencia/repository/ConvenioRepository.java
@@ -0,0 +1,10 @@
+package com.codenine.projetotransparencia.repository;
+
+import com.codenine.projetotransparencia.entities.Convenio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ConvenioRepository extends JpaRepository<Convenio, Long> {
+
+}
