2d75f1a47e32e64c0a6750e7681961cd0e488696
JonasOliver <jonaasmigueldeoliveira@gmail.com>
2024-11-26 10:21:19 -0300

    Fix: Atualizei o controller de prestação de contas

--- projetotransparencia/src/main/java/com/codenine/projetotransparencia/controllers/PrestacaoContasController.java
+++ projetotransparencia/src/main/java/com/codenine/projetotransparencia/controllers/PrestacaoContasController.java
@@ -5,6 +5,7 @@ import com.codenine.projetotransparencia.services.PrestacaoContasService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,12 +17,21 @@ public class PrestacaoContasController {
     @Autowired
     private PrestacaoContasService prestacaoContasService;
 
+    // Endpoint que retorna todas as prestações de contas
     @GetMapping("/listar")
     public List<PrestacaoContas> listarPrestacaoContas() {
         List<PrestacaoContas> prestacoesContas = prestacaoContasService.listarPrestacaoContas();
         return prestacoesContas;
     }
 
+    // Novo endpoint que aceita um parâmetro de título de projeto
+    @GetMapping("/listar-por-titulo")
+    public List<PrestacaoContas> listarPrestacaoContasPorTituloProjeto(@RequestParam String titulo) {
+        List<PrestacaoContas> prestacoesContas = prestacaoContasService.listarPrestacaoContasPorTituloProjeto(titulo);
+        return prestacoesContas;
+    }
+
+    // Endpoint que retorna uma prestação de contas por ID
     @GetMapping("/visualizar/{id}")
     public PrestacaoContas visualizarPrestacaoContas(Long id) {
         PrestacaoContas prestacaoContas = prestacaoContasService.visualizarPrestacaoContas(id);
