113cf2ee838dd1a19c8e6bb267556c433e11e414
JonasOliver <jonaasmigueldeoliveira@gmail.com>
2024-11-26 10:21:46 -0300

    Fix: Atualizei o gasto controller

--- projetotransparencia/src/main/java/com/codenine/projetotransparencia/controllers/GastoController.java
+++ projetotransparencia/src/main/java/com/codenine/projetotransparencia/controllers/GastoController.java
@@ -4,10 +4,8 @@ import com.codenine.projetotransparencia.controllers.dto.CadastrarGastoDto;
 import com.codenine.projetotransparencia.services.GastoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -16,6 +14,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/gasto")
 public class GastoController {
+
     @Autowired
     private GastoService gastoService;
 
@@ -23,13 +22,13 @@ public class GastoController {
     public ResponseEntity<?> cadastrarGasto(
             @RequestParam(value = "gasto") String gasto,
             @RequestParam(value = "idProjeto") Long projetoId,
-            @RequestParam(required = false, value = "notaFiscal")MultipartFile notaFiscal) {
+            @RequestParam(required = false, value = "notaFiscal") MultipartFile notaFiscal) {
 
         try {
             CadastrarGastoDto cadastrarGastoDto = new CadastrarGastoDto(
-                gasto,
-                projetoId,
-                Optional.ofNullable(notaFiscal)
+                    gasto,
+                    projetoId,
+                    Optional.ofNullable(notaFiscal)
             );
 
             gastoService.cadastrarGasto(cadastrarGastoDto);
@@ -39,6 +38,38 @@ public class GastoController {
             return ResponseEntity.badRequest().body(e.getMessage());
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    @PutMapping("/editar/{id}")
+    public ResponseEntity<?> editarGasto(@PathVariable Long id,
+                                         @RequestParam(value = "gasto") String gasto,
+                                         @RequestParam(value = "idProjeto") Long projetoId,
+                                         @RequestParam(required = false, value = "notaFiscal") MultipartFile notaFiscal) {
+        try {
+            CadastrarGastoDto cadastrarGastoDto = new CadastrarGastoDto(
+                    gasto,
+                    projetoId,
+                    Optional.ofNullable(notaFiscal)
+            );
+
+            gastoService.editarGasto(id, cadastrarGastoDto);
+
+            return ResponseEntity.ok(null);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @DeleteMapping("/excluir/{id}")
+    public ResponseEntity<?> excluirGasto(@PathVariable Long id) {
+        try {
+            gastoService.excluirGasto(id);
+            return ResponseEntity.ok("Gasto excluído com sucesso.");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 }
