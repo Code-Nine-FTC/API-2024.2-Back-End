5f758b3a0bbb842698fc253efc5a4330bcf658bc
JonasOliver <jonaasmigueldeoliveira@gmail.com>
2024-11-30 22:48:52 -0300

    Fix: Atualização AtualizarConvenioDto

--- projetotransparencia/src/main/java/com/codenine/projetotransparencia/controllers/dto/AtualizarConvenioDto.java
+++ projetotransparencia/src/main/java/com/codenine/projetotransparencia/controllers/dto/AtualizarConvenioDto.java
@@ -2,11 +2,11 @@ package com.codenine.projetotransparencia.controllers.dto;
 
 import java.util.Optional;
 
-public record  AtualizarConvenioDto (
+public record AtualizarConvenioDto(
         Optional<String> nome,
         Optional<String> cnpj,
         Optional<String> email,
         Optional<String> telefone,
-        Optional<String> areaColaboracao
-) {
-}
+        Optional<String> areaColaboracao,
+        Optional<String> historicoParceria
+) {}
